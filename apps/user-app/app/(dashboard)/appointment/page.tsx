7abe5122ae465a64be714b6765fb2dbe840c098e
AyushShivhare79 <ayushshivhare1008@gmail.com>
2024-05-25 23:26:17 +0530

    init user panel

---
+++ apps/user-app/app/(dashboard)/appointment/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <>
+      <div>Appointment</div>
+    </>
+  );
+};
+
+export default Page;
---
+++ apps/user-app/app/(dashboard)/layout.tsx
@@ -0,0 +1,8 @@
+export default function ({ children }: any) {
+  return (
+    <div>
+      <div></div>
+      {children}
+    </div>
+  );
+}

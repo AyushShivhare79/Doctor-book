d3934c32b419788e565edc9284dfe0990eb1ebc0
AyushShivhare79 <ayushshivhare1008@gmail.com>
2024-07-26 14:58:03 +0530

    fix: added image url in card

--- apps/user-app/app/components/Card.tsx
+++ apps/user-app/app/components/Card.tsx
@@ -1,4 +1,14 @@
+import Image from "next/image";
 import Button from "./Button";
+// import { Card } from "@repo/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@repo/ui/card";
 
 export default function ({
   data,
@@ -12,27 +22,73 @@ export default function ({
   return (
     <>
       {data.map((d: any) => (
-        <div className="border border-black rounded-lg p-10">
-          <div>Image</div>
-          <div>
-            {d.firstName} {d.lastName}
-          </div>
+        <Card>
+          <CardHeader>
+            <CardTitle>
+              <img
+                className="max-w-[250px] min-w-[250px] max-h-[250px] min-h-[250px] object-cover"
+                src={d.image}
+                alt=""
+              />
+            </CardTitle>
+            <CardDescription></CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className=" text-2xl font-mono">
+              {d.firstName} {d.lastName}
+            </div>
+            <div className="flex gap-2">
+              <div>{Category}</div>
+              <div>HARDCODED</div>
+            </div>
 
-          <div className="flex gap-2">
-            <div>{Category}</div>
-            <div>{d.category}</div>
-          </div>
+            <div className="flex gap-2">
+              <div>{AddIcon}</div>
+              <div>{d.address}</div>
+            </div>
 
-          <div className="flex gap-2">
-            <div>{AddIcon}</div>
-            <div>{d.address}</div>
-          </div>
+            <div className="flex gap-5">
+              <div>Fees: {d.fees} Rs</div>
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button id={d.id} btnName="BOOK NOW" />
+          </CardFooter>
+        </Card>
 
-          <div className="flex gap-5">
-            <div>Fees: {d.fees} Rs</div>
-            <Button id={d.id} btnName="BookNow" />
-          </div>
-        </div>
+        // <div className="border border-black rounded-lg p-10">
+        //   <div>
+        //     {/* <Image
+        //       src={d.image}
+        //       width={500}
+        //       height={500}
+        //       alt="Picture of the author"
+        //     />*/}
+        //     <img
+        //       className="max-w-[200px] min-w-[200px] max-h-[200px] min-h-[200px] object-cover"
+        //       src={d.image}
+        //       alt=""
+        //     />
+        //   </div>
+        //   <div>
+        //     {d.firstName} {d.lastName}
+        //   </div>
+
+        //   <div className="flex gap-2">
+        //     <div>{Category}</div>
+        //     <div>{d.category}</div>
+        //   </div>
+
+        //   <div className="flex gap-2">
+        //     <div>{AddIcon}</div>
+        //     <div>{d.address}</div>
+        //   </div>
+
+        //   <div className="flex gap-5">
+        //     <div>Fees: {d.fees} Rs</div>
+        //     <Button id={d.id} btnName="BookNow" />
+        //   </div>
+        // </div>
       ))}
     </>
   );
